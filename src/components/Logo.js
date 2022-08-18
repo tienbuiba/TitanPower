bc003ab901cb3d7b4bb325e7038fda99488e900c
tienbuiba <buibatien2408@gmail.com>
2022-08-18 10:00:35 +0700

    change route logo continue

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,8 @@
-import ApolloLogo from '../assets/images/logo/logo.png';
 import {React} from 'react';
 
 const Logo = () => {
     return (
-        <img src={ApolloLogo} alt="Apollo" width="100%" height="60px" 
+        <img src={require('../assets/images/logo/logo.png')} alt="Apollo" width="100%" height="60px" 
         style={{
             opacity: '1',
             transition: 'opacity .4s'
