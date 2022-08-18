c015115469056375a86a448a72cbfac1e5c8219a
tienbuiba <buibatien2408@gmail.com>
2022-08-18 10:06:35 +0700

    a

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import {React} from 'react';
 
 const Logo = () => {
     return (
-        <img src={require('../assets/images/logo/logo.png')} alt="Apollo" width="100%" height="60px" 
+        <img src={require('./../assets/images/logo/logo.png')} alt="Apollo" width="100%" height="60px" 
         style={{
             opacity: '1',
             transition: 'opacity .4s'
